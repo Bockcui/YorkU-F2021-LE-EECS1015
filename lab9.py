fd599dacf4b04f8b17b2209233aada8a584fdda6
Bockcui <49225131+Bockcui@users.noreply.github.com>
2022-01-10 17:21:57 -0500

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -1,13 +1,3 @@
-###################
-#
-# Lab 9
-# Author: Cheng Tian Cui
-# Email: cttcui@my.yorku.ca
-# Student ID: 218082305
-# Section A
-#
-###################
-
 from MinMaxList import MinMaxList
 from random import randint
 
